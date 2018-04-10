b6f3ff6e85556b6c10da1195ff594ae24ba3d03b
Jens Kieselbach <jens-w.kieselbach@gmx.de>
2018-04-10 20:27:10 +0200

    Work in progress

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # qtIFM
-qtIFMis a simple graphical interface to IFM.
+qtIFM is a simple graphical interface to IFM.
 
 It's written in python using Qt5 as GUI toolkit. It's meant to be a modern replacement for tkifm
 
--- qtifm/gui.py
+++ qtifm/gui.py
@@ -19,7 +19,7 @@ from PyQt5.QtCore import pyqtSlot, Qt, QSize, QThread
 from PyQt5.QtWidgets import (QAbstractItemView, QAction, QCheckBox, QDialog, QFileDialog, QHBoxLayout, QLabel,
                              QListWidget, QListWidgetItem, QMainWindow, QPlainTextEdit, QPushButton, QSizePolicy,
                              QSplitter, QVBoxLayout, QWidget, QFrame, QDialogButtonBox, QGridLayout, QLineEdit,
-                             QMessageBox, QTableWidget, QTableWidgetItem, QListView, QLayout)
+                             QMessageBox, QTextEdit, QTableWidgetItem, QListView, QLayout)
 
 images_path = Path(__file__).parent.joinpath('images')
 resources_path = Path(__file__).parent.joinpath('resources')
@@ -100,13 +100,25 @@ class MainWindow(QMainWindow):
         # Actions
         self.exit_action = QAction(_('Exit'), self)
         self.exit_action.setMenuRole(QAction.QuitRole)
+        self.exit_action.setShortcut('Ctrl+Q')
         self.about_action = QAction(_('About'), self)
+        self.about_action.setMenuRole(QAction.AboutRole)
 
         self.new_action = QAction(QIcon.fromTheme('document-new'), _('New'))
+        self.new_action.setShortcut('Ctrl+N')
+        self.open_action = QAction(QIcon.fromTheme('document-open'), _('Open...'))
+        self.open_action.setShortcut('Ctrl+O')
+        self.save_action = QAction(QIcon.fromTheme('document-save'), _('Save'))
+        self.save_action.setShortcut('Ctrl+S')
+        self.saveas_action = QAction(QIcon.fromTheme('document-save-as'), _('Save As...'))
+        self.saveas_action.setShortcut('Shift+Ctrl+S')
 
         # Menu Bar
         file_menu = self.menuBar().addMenu(_('File'))
         file_menu.addAction(self.new_action)
+        file_menu.addAction(self.open_action)
+        file_menu.addAction(self.save_action)
+        file_menu.addAction(self.saveas_action)
         file_menu.addSeparator()
         file_menu.addAction(self.exit_action)
 
@@ -118,9 +130,18 @@ class MainWindow(QMainWindow):
         tool_bar.setFloatable(False)
         tool_bar.setMovable(False)
         tool_bar.addAction(self.new_action)
+        tool_bar.addAction(self.open_action)
+        tool_bar.addAction(self.save_action)
+        tool_bar.addAction(self.saveas_action)
+
+        # self.statusBar().setStyleSheet('margin-bottom: 2px;')
+        self.statusBar().showMessage('Ready')
 
         # Widgets
-        # self.splitter = QSplitter(Qt.Horizontal)
+        self.splitter = QSplitter(Qt.Horizontal)
+        self.text_edit = QTextEdit()
+        self.text_edit.setStyleSheet('font-family: "Monospace";')
+
         # self.rp9_viewer = Rp9Viewer(self.config)
         # self.file_list = QListWidget()
         # self.file_list.setSelectionMode(QAbstractItemView.SingleSelection)
@@ -135,15 +156,18 @@ class MainWindow(QMainWindow):
         # self.show_hidden_check.stateChanged.connect(self.update_dir)
         # self.file_list.itemDoubleClicked.connect(self.show_file)
 
+        self.text_edit.cursorPositionChanged.connect(self.cursor_position_changed)
+
         # Layout
-        # self.setCentralWidget(self.splitter)
+        self.setCentralWidget(self.splitter)
 
-        # left_widget = QWidget()
-        # self.splitter.addWidget(left_widget)
-        # left_layout = QVBoxLayout()
-        # left_widget.setLayout(left_layout)
+        left_widget = QWidget()
+        # left_widget.setStyleSheet('margin: 0px;')
+        self.splitter.addWidget(left_widget)
+        left_layout = QVBoxLayout()
+        left_widget.setLayout(left_layout)
 
-        # left_layout.addWidget(self.dir_button)
+        left_layout.addWidget(self.text_edit)
         # left_layout.addWidget(self.file_list)
         # left_layout.addWidget(self.show_hidden_check)
 
@@ -153,6 +177,12 @@ class MainWindow(QMainWindow):
     def show_about_dialog(self):
         dialog = AboutDialog(self)
         dialog.exec_()
+
+    @pyqtSlot()
+    def cursor_position_changed(self):
+        cursor = self.text_edit.textCursor()
+        self.statusBar().showMessage(
+            'Line: ' + str(cursor.blockNumber() + 1) + ', Column: ' + str(cursor.columnNumber() + 1))
 
     def closeEvent(self, event):
         self.config.mainwindow_witdh = self.width()
